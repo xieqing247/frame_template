8cb9f8ebf392f8e7c02c4011a42401b745210c2c
xieqing <476743527qq@.com>
2022-05-16 16:42:15 +0800

    完善框架

---
+++ elasticsearch/src/main/java/org/xq/es/XqEsApplication.java
@@ -0,0 +1,11 @@
+package org.xq.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XqEsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XqEsApplication.class,args);
+    }
+}
