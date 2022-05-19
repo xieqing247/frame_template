333eefc143d281197c356dca448c46fceb442ac5
xieqing <476743527qq@.com>
2022-05-19 18:16:37 +0800

    完善框架

--- boot/src/main/java/org/example/boot/BootApplication.java
+++ boot/src/main/java/org/example/boot/BootApplication.java
@@ -1,9 +1,13 @@
 package org.example.boot;
 
+import org.example.boot.utils.AopTargetUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @SpringBootApplication
@@ -11,9 +15,23 @@ public class BootApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(BootApplication.class, args);
-        String[] beans = context.getBeanNamesForAnnotation(RestController.class);
+        String[] beans = context.getBeanNamesForAnnotation(Controller.class);
         for (String bean : beans) {
             System.out.println(bean);
+        }
+
+        List<Object> list = context.getBeansWithAnnotation(Controller.class).values().parallelStream().map(e -> {
+            try {
+                return AopTargetUtils.getTarget(e);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+            return e;
+        }).collect(Collectors.toList());
+
+
+        for (Object o : list) {
+            System.out.println(o);
         }
     }
 
